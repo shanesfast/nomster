7969a87e45b79dd4f1867bc84931671dbe81aec8
Shane Quick <shanesfast@gmail.com>
2019-09-11 14:59:30 +0000

    Add validations for name, address, description fields.

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,5 @@
 class Place < ApplicationRecord
     belongs_to :user
-    validates :name, presence: true
+    validates :name, :address, presence: true, length: { in: 3..50 }
+    validates :description, presence: true, length: { in: 3..300 }
 end
