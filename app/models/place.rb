2cdc1864ba00c385415894c5a848bc738281eda0
Shane Quick <shanesfast@gmail.com>
2019-09-17 00:10:30 +0000

    Set up database for comments

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,10 @@
 class Place < ApplicationRecord
     belongs_to :user
+    has_many :comments
+
     geocoded_by :address
     after_validation :geocode
+    
     validates :name, :address, presence: true, length: { in: 3..50 }
     validates :description, presence: true, length: { in: 3..300 }
 end
