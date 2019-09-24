95687395020d21fb3a62914a9f77b938374b01ff
Shane Quick <shanesfast@gmail.com>
2019-09-24 22:43:48 +0000

    Add dynamic content to e-mail templates

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,9 +1,10 @@
 class NotificationMailer < ApplicationMailer
-  default from: "no-reply@nomster-app.com"
+  default from: "no-reply@nomsterapp.com"
 
-  def comment_added
-    mail(to: "shanesfast@gmail.com",
-        subject: "You have a new comment to read!")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email, subject: "You have a new comment on #{@place.name}!")
   end
 
 end
