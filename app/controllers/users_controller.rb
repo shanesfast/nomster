469abb3f812b83dad096db69cab210846b8b1b04
Shane Quick <shanesfast@gmail.com>
2019-10-03 03:26:43 +0000

    Added pagination for user profile comments and places

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @pagy_comments, @comments = pagy(@user.comments, page_param: :page_comments, items: 4)
+    @pagy_places, @places = pagy(@user.places, page_param: :page_places, items: 5)
   end
 end
